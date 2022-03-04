50d04053849a909096f2b2852629069a87ea4d6f
potoi <90742431+potoi@users.noreply.github.com>
2022-03-04 17:55:07 -0300

    Testandoooooooo

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app= express();
+const fs = require ('fs');
+app.get('/',(re,res)=>{
+
+
+  fs.readFile('home.html','utf-8',(err,data)=>{
+
+    if(err){
+      console.log(err)
+      return
+    }
+    console.log(data)
+    res.send(data);
+  
+  })
+
+
+});
+
+
+
+app.listen(process.env.PORT || 3000,() => console.log("https//localhost:3000"));
